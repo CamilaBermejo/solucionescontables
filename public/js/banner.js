55b21a827c40278d2c929ba077b6626ffcf05562
JoaquinCostamagna <jncostamagna@gmail.com>
2022-05-06 13:28:40 -0300

    Implementación de banner

---
+++ public/js/banner.js
@@ -0,0 +1,11 @@
+const slides = document.querySelectorAll(".slide");
+
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById("bannerRadio" + counter).checked = true;
+    counter++;
+    if(counter > slides.length){
+        counter = 1;
+    }
+}, 5000)
