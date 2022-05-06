ce3a339b8ff868cdd82af948af83720267aac8ae
JoaquinCostamagna <jncostamagna@gmail.com>
2022-05-06 19:28:28 -0300

    Inclusión de banner dentro de main

--- public/js/banner.js
+++ public/js/banner.js
@@ -2,10 +2,10 @@ const slides = document.querySelectorAll(".slide");
 
 
 var counter = 1;
-setInterval(function(){
-    document.getElementById("bannerRadio" + counter).checked = true;
-    counter++;
-    if(counter > slides.length){
-        counter = 1;
-    }
-}, 5000)
+// setInterval(function(){
+//     document.getElementById("bannerRadio" + counter).checked = true;
+//     counter++;
+//     if(counter > slides.length){
+//         counter = 1;
+//     }
+// }, 5000)
