3436385d4ed3d08587b9beadded658ad6705d167
JoaquinCostamagna <jncostamagna@gmail.com>
2022-05-08 18:49:54 -0300

    Implementación de sliding automático

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,19 +1,33 @@
-const manualButtons = document.querySelectorAll(".manual-btn");
+const labelButtons = document.querySelectorAll(".manual-btn");
+const radioButtons = document.querySelectorAll(".banner-radio-btn");
+var currentBanner = 0;
+var intervalTime = 5000;
 
-manualButtons.forEach(button => {
+labelButtons.forEach(button => {
     button.addEventListener('click', (e)=>{
         document.querySelector('.checked').classList.remove('checked');
         e.currentTarget.classList.add('checked');
+        currentBanner = e.currentTarget.getAttribute('data-label-id');
+        intervalTime = 10000;
+        resetTimeoutTime(10000);
     })
 })
 
+nextBanner();
 
+function resetTimeoutTime(time){
+    var timeoutTime = setTimeout(()=>{
+        intervalTime = 5000;
+    },time);
+}
 
-var counter = 1;
-// setInterval(function(){
-//     document.getElementById("bannerRadio" + counter).checked = true;
-//     counter++;
-//     if(counter > slides.length){
-//         counter = 1;
-//     }
-// }, 5000)
+function nextBanner(){
+    var bannerTimer = setTimeout(()=>{
+        if(currentBanner == radioButtons.length) currentBanner = 0;
+        document.querySelector('.checked').classList.remove('checked');
+        labelButtons[currentBanner].classList.add('checked');
+        radioButtons[currentBanner].checked = true;
+        currentBanner++;
+        nextBanner();
+    }, intervalTime);
+}
